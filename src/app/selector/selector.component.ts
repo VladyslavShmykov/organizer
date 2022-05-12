a07ebdd35ffd05e9dbf548636346d7d7bb4d5a90
vladyslav <vladislavshmuk@gmail.com>
2022-05-12 12:47:00 +0300

    selector

---
+++ src/app/selector/selector.component.ts
@@ -0,0 +1,24 @@
+import {Component} from '@angular/core';
+import {DateService} from "../shared/services/date.service";
+import * as moment from "moment";
+
+@Component({
+  selector: 'app-selector',
+  templateUrl: './selector.component.html',
+  styleUrls: ['./selector.component.scss']
+})
+export class SelectorComponent {
+
+  constructor(
+    private dateService: DateService,
+  ) {
+  }
+
+  public get getDate(): moment.Moment {
+    return this.dateService.date.getValue();
+  }
+
+  public onChangeDate(dir: number): void {
+    this.dateService.changeMonth(dir);
+  }
+}
